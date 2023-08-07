a32f682e4acd0fbdfe434e2f2572a8a595d378b7
NotVinnny <vh222@cornell.edu>
2023-08-06 21:02:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# strophe-mobile
+ strophe but mobile
---
+++ strophe/android/app/src/main/kotlin/com/example/strophe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.strophe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
