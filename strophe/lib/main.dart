18570e591a188de0ea8b4a028ff7028ef8c0a021
NotVinnny <vh222@cornell.edu>
2023-08-13 05:16:14 -0400

    Shuffle icon
    +added shuffle icon
    -removed background shadow on shuffle

--- strophe/lib/main.dart
+++ strophe/lib/main.dart
@@ -10,6 +10,7 @@ class MyScaffold extends StatelessWidget {
   Future<Map<String, dynamic>> fetchRandomPoem() async {
     final response = await http.get(Uri.parse("https://poetrydb.org/random"));
 
+    print("Grabbing new poems");
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       return data[0];
@@ -21,66 +22,74 @@ class MyScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Strophe"),
-          backgroundColor: Color.fromRGBO(57, 54, 70, 1.0),
-          centerTitle: true,
-        ),
-        body: FutureBuilder(
-            future: fetchRandomPoem(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
-                    child:
-                        CircularProgressIndicator()); // display centered loading circle if data not ready for display
-              }
-              if (snapshot.hasData) {
-                final poem = snapshot.data;
-                final title = poem!['title'];
-                final author = poem['author'];
-                final lines = poem['lines'];
-                var content = '';
+      appBar: AppBar(
+        title: Text("Strophe"),
+        backgroundColor: Color.fromRGBO(57, 54, 70, 1.0),
+        centerTitle: true,
+      ),
+      body: FutureBuilder(
+          future: fetchRandomPoem(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                  child:
+                      CircularProgressIndicator()); // display centered loading circle if data not ready for display
+            }
+            if (snapshot.hasData) {
+              final poem = snapshot.data;
+              final title = poem!['title'];
+              final author = poem['author'];
+              final lines = poem['lines'];
+              var content = '';
 
-                for (int i = 0; i < lines.length; i++) {
-                  content += lines[i] + "\n";
-                }
-                return SingleChildScrollView(
-                  child: Center(
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        left: 10,
-                        right: 10,
-                      ),
-                      child: Column(children: [
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Text(
-                          "$title",
-                          style: TextStyle(fontSize: 32),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Text(
-                          "$author",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        SizedBox(
-                          height: 16,
-                        ),
-                        Text(
-                          content,
-                          style: TextStyle(fontSize: 24),
-                        ),
-                      ]),
-                    ),
-                  ),
-                );
-              } else {
-                return Center(child: CircularProgressIndicator());
+              for (int i = 0; i < lines.length; i++) {
+                content += lines[i] + "\n";
               }
-            }));
+              return SingleChildScrollView(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                    ),
+                    child: Column(children: [
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        "$title",
+                        style: TextStyle(fontSize: 32),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        "$author",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        content,
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ]),
+                  ),
+                ),
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
+      bottomNavigationBar: Container(
+          height: 50,
+          color: Colors.grey[200],
+          child: IconButton(
+            icon: Icon(Icons.shuffle),
+            onPressed: fetchRandomPoem,
+          )),
+    );
   }
 }
 
@@ -89,5 +98,10 @@ void main() {
     // wrapping with MaterialApp widget is crucial or else code breaks
     home: MyScaffold(),
     title: "Strophe",
+    theme: ThemeData(
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+      hoverColor: Colors.transparent,
+    ),
   ));
 }
