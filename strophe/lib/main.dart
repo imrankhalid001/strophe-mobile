7a4e16666dc8adc36a21d69d192c6f03fef39025
NotVinnny <vh222@cornell.edu>
2023-08-16 07:06:04 -0400

    Added functionality to shuffle button
    + shuffle button now works as intended

--- strophe/lib/main.dart
+++ strophe/lib/main.dart
@@ -28,90 +28,113 @@ class MyScaffold extends StatelessWidget {
         centerTitle: true,
       ),
       body: PoemWidget(),
-      bottomNavigationBar: FooterWidget(),
     );
   }
 }
 
-class FooterWidget extends StatelessWidget {
-  const FooterWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        height: 50,
-        color: Colors.grey[200],
-        child: IconButton(
-          icon: Icon(Icons.shuffle),
-          onPressed: fetchRandomPoem,
-        ));
-  }
-}
-
-class PoemWidget extends StatelessWidget {
+class PoemWidget extends StatefulWidget {
   const PoemWidget({
     super.key,
   });
 
   @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: fetchRandomPoem(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-                child:
-                    CircularProgressIndicator()); // display centered loading circle if data not ready for display
-          }
-          if (snapshot.hasData) {
-            final poem = snapshot.data;
-            final title = poem!['title'];
-            final author = poem['author'];
-            final lines = poem['lines'];
-            var content = '';
+  State<PoemWidget> createState() => _PoemWidgetState();
+}
 
-            for (int i = 0; i < lines.length; i++) {
-              content += lines[i] + "\n";
+class _PoemWidgetState extends State<PoemWidget> {
+  var poem = '';
+  var title = '';
+  var author = '';
+  var lines = '';
+  var content = '';
+
+  Future<void> _fetchRandomPoem() async {
+    final poemData = await fetchRandomPoem(); // new API call to grab new poem
+    final title = poemData['title'];
+    final author = poemData['author'];
+    final lines = poemData['lines'];
+
+    var content = '';
+
+    for (int i = 0; i < lines.length; i++) {
+      content += lines[i] + "\n";
+    }
+
+    setState(() {
+      // updating existing/parent fields with values from new call
+      this.title = title;
+      this.author = author;
+      this.content = content;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+          future: fetchRandomPoem(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                  child:
+                      CircularProgressIndicator()); // display centered loading circle if data not ready for display
             }
-            return SingleChildScrollView(
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.only(
-                    left: 10,
-                    right: 10,
+            if (snapshot.hasData) {
+              final poem = snapshot.data;
+              final title = poem!['title'];
+              final author = poem['author'];
+              final lines = poem['lines'];
+              var content = '';
+
+              for (int i = 0; i < lines.length; i++) {
+                content += lines[i] + "\n";
+              }
+              return SingleChildScrollView(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      left: 10,
+                      right: 10,
+                    ),
+                    child: Column(children: [
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        "$title",
+                        style: TextStyle(fontSize: 32),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        "$author",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        content,
+                        style: TextStyle(fontSize: 24),
+                      ),
+                    ]),
                   ),
-                  child: Column(children: [
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      "$title",
-                      style: TextStyle(fontSize: 32),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      "$author",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    SizedBox(
-                      height: 16,
-                    ),
-                    Text(
-                      content,
-                      style: TextStyle(fontSize: 24),
-                    ),
-                  ]),
                 ),
-              ),
-            );
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        });
+              );
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          }),
+      bottomNavigationBar: Container(
+          height: 50,
+          color: Colors.grey[200],
+          child: IconButton(
+            icon: Icon(Icons.shuffle),
+            onPressed:
+                _fetchRandomPoem, // when pressed, reference function to grab from API and update UI
+          )),
+    );
   }
 }
 
