10fc5fb2b606a12c1eee1bb317db05f369bf877c
NotVinnny <vh222@cornell.edu>
2023-08-20 12:07:40 -0400

    Updated poem schema

--- strophe/lib/model/poem.dart
+++ strophe/lib/model/poem.dart
@@ -3,9 +3,10 @@
 final String tablePoems = "poems";
 
 class PoemFields {
-  static final List<String> values = [id, title, author, content];
+  static final List<String> values = [id, isFavorite, title, author, content];
 
   static final String id = "_id";
+  static final String isFavorite = 'isFavorite';
   static final String title = "title";
   static final String author = "author";
   static final String content = "content";
@@ -13,12 +14,14 @@ class PoemFields {
 
 class Poem {
   final int? id;
+  final bool isFavorite;
   final String title;
   final String author;
   final String content;
 
   const Poem({
     this.id,
+    required this.isFavorite,
     required this.title,
     required this.author,
     required this.content,
@@ -26,12 +29,14 @@ class Poem {
 
   Poem copy({
     int? id,
+    bool? isFavorite,
     String? title,
     String? author,
     String? content,
   }) =>
       Poem(
         id: id ?? this.id,
+        isFavorite: isFavorite ?? this.isFavorite,
         title: title ?? this.title,
         author: author ?? this.author,
         content: content ?? this.content,
@@ -39,6 +44,7 @@ class Poem {
 
   static Poem fromJson(Map<String, Object?> json) => Poem(
         id: json[PoemFields.id] as int?,
+        isFavorite: json[PoemFields.isFavorite] == 1,
         title: json[PoemFields.title] as String,
         author: json[PoemFields.author] as String,
         content: json[PoemFields.content] as String,
@@ -46,6 +52,7 @@ class Poem {
 
   Map<String, Object?> toJson() => {
         PoemFields.id: id,
+        PoemFields.isFavorite: isFavorite ? 1 : 0,
         PoemFields.title: title,
         PoemFields.author: author,
         PoemFields.content: content,
